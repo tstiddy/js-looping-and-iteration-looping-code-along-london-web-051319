f2bbcf1df656c00d4106c105d09d0ca7f729efe5
tstiddy <spaceinvadini99@gmail.com>
2019-06-24 11:18:12 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+// const namesArray = ["Ada", "Brendan", "Ali"];
+// const event = ();
+
+function writeCards( namesArray, event) {
+  let thankYouCards = []
+  for (let i = 0; i < namesArray.length; i++) {
+    thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    // console.log('Thank you ${namesArray[i]}, for the wondeful ${event} gift.')
+  }
+
+  return thankYouCards
+}
+
+function countdown() {
+  let countDown = 11;
+  while (countDown > 0) {
+    console.log(countDown -=1);
+  }
+}
